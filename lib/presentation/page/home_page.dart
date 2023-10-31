6fe4b00cd4d72bc22b9aafbec8aeb8fdac7db9ca
rzkzx <romanzidanramadhan@gmail.com>
2023-10-31 17:04:32 +0800

    homepage ui

--- lib/presentation/page/home_page.dart
+++ lib/presentation/page/home_page.dart
@@ -1,33 +1,150 @@
-import 'package:d_info/d_info.dart';
+import 'package:d_view/d_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:money_record/config/session.dart';
-import 'package:money_record/presentation/page/auth/login_page.dart';
+import 'package:money_record/config/app_asset.dart';
+import 'package:money_record/config/app_color.dart';
+import 'package:money_record/presentation/controller/c_user.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final cUser = Get.put(CUser());
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Row(
-          children: [
-            const Text('Home Page'),
-            IconButton(
-              onPressed: () {
-                Session.clearUser();
-                DInfo.dialogSuccess('Berhasil Logout');
-                DInfo.closeDialog(
-                  actionAfterClose: () {
-                    Get.off(() => const LoginPage());
-                  },
-                );
-              },
-              icon: const Icon(Icons.logout),
+      endDrawer: const Drawer(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 50, 20, 30),
+            child: Row(
+              children: [
+                Image.asset(AppAsset.profile),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Hi,',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Obx(() {
+                        return Text(
+                          cUser.data.name ?? '',
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        );
+                      }),
+                    ],
+                  ),
+                ),
+                Builder(builder: (ctx) {
+                  return Material(
+                    borderRadius: BorderRadius.circular(5),
+                    color: AppColor.chart,
+                    child: InkWell(
+                      onTap: () {
+                        Scaffold.of(ctx).openEndDrawer();
+                      },
+                      borderRadius: BorderRadius.circular(5),
+                      child: const Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Icon(
+                          Icons.menu,
+                          color: AppColor.primary,
+                        ),
+                      ),
+                    ),
+                  );
+                })
+              ],
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.fromLTRB(20, 0, 20, 30),
+              children: [
+                Text(
+                  'Pengeluaran Hari Ini',
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                DView.spaceHeight(),
+                Material(
+                  borderRadius: BorderRadius.circular(16),
+                  elevation: 5,
+                  color: AppColor.primary,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(16, 20, 16, 5),
+                        child: Text(
+                          'Rp 123.000,00',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineMedium!
+                              .copyWith(
+                                fontWeight: FontWeight.bold,
+                                color: AppColor.secondary,
+                              ),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(16, 0, 16, 25),
+                        child: Text(
+                          '+20% dibanding kemarin',
+                          style: TextStyle(
+                            color: AppColor.bg,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(16, 0, 0, 16),
+                        padding: const EdgeInsets.symmetric(vertical: 2),
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(8),
+                            bottomLeft: Radius.circular(8),
+                          ),
+                        ),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              'Selengkapnya',
+                              style: TextStyle(
+                                color: AppColor.primary,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Icon(
+                              Icons.navigate_next,
+                              color: AppColor.primary,
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
